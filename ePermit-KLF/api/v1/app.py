2babda4351038fa496127537fc5441724854162a
DianaWangui <dianawanguikt048@gmail.com>
2024-05-06 18:42:00 +0300

    add user api route, created the app_views blueprint, updated dbstorage, updated basemode to handle the user session and the to_dict method appropriately

--- ePermit-KLF/api/v1/app.py
+++ ePermit-KLF/api/v1/app.py
@@ -8,16 +8,25 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views) # Register the blueprint
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}}) # Enable CORS
 
+
+#This that closes the db session
 @app.teardown_appcontext
 def close_db(exception=None):
     """method that closed db session"""
     storage.close()
 
 
+# Error handler for 404 (page not found error)
+@app.errorhandler(404)
+def not_found(error):
+    """method that handle 404 error"""
+    return jsonify({"error": "Page Not found"}), 404
+
+
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", 5000)
+    host = getenv("EPERMIT_API_HOST", "0.0.0.0")
+    port = getenv("EPERMIT_API_PORT", 5000)
     app.run(host=host, port=port, threaded=True, debug=True)
---
+++ ePermit-KLF/api/v1/views/users.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+""" Module for the API  users """
+from api.v1.views import app_views
+from flask import jsonify, request, abort
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    """ method that retrieves all users """
+    users = storage.all(User).values()
+    users_list = []
+    users_list = [user.to_dict() for user in users]
+    return jsonify(users_list)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """ method that retrieves a user """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id):
+    """ method that deletes a user """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """ method that creates a user """
+    user_json = request.get_json()
+    if user_json is None:
+        abort(400, "Not a JSON")
+    if 'email' not in user_json:
+        abort(400, "Missing email")
+    if 'password' not in user_json:
+        abort(400, "Missing password")
+    user = User(**user_json)
+    storage.new(user)
+    storage.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """ method that updates a user """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    user_json = request.get_json()
+    if user_json is None:
+        abort(400, "Not a JSON")
+    for key, value in user_json.items():
+        if key not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
