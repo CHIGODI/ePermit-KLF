08df489ac0ee9490b92a080d1e1741f05e63b083
CHIGODI <chiegody254@gmail.com>
2024-05-27 13:48:57 +0300

    Add payment code to flask app main

--- api/v1/views/mpesa.py
+++ api/v1/views/mpesa.py
@@ -16,60 +16,6 @@ from api.v1.views import app_views
 from os import getenv
 
 
-# get access token to work with daraja API
-def get_access_token(consumer_key, consumer_secret):
-    """ Get access token to work with daraja API """
-    url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
-    response = requests.get(url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
-    if response.status_code == 200:
-        return response.json().get('access_token')
-    else:
-        return None
-
-
-@app_views.route('/pay', methods=['POST'], strict_slashes=False)
-# @token_required
-def mpesa_express():
-    """ This function initiates a payment request to the M-Pesa API. """
-    if request.method == 'POST':
-        access_token = get_access_token(getenv('CONSUMER_KEY'),
-                                        getenv('CONSUMER_SECRET'))
-        time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        password = "174379" + "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" + time_stamp
-        pwd = b64encode(password.encode("utf-8")).decode("utf-8")
-        phone_number = request.form.get('phone_number')
-
-        if not access_token:
-            return jsonify({"status": "Failed"})
-
-        headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {access_token}'
-        }
-        payload = {
-        "BusinessShortCode": 174379,
-        "Password": pwd,
-        "Timestamp": time_stamp,
-        "TransactionType": "CustomerPayBillOnline",
-        "Amount": 1,
-        "PartyA": phone_number,
-        "PartyB": 174379,
-        "PhoneNumber": phone_number,
-        "CallBackURL": "https://epermit.live/api/v1/callback",
-        "AccountReference": "CompanyXLTD",
-        "TransactionDesc": "Payment of X"
-        }
-        payload_json = json.dumps(payload)
-        response = requests.request("POST",
-                                    'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest',
-                                    headers = headers,
-                                    data = payload_json)
-        response_code = response.json().get('ResponseCode')
-        print(response_code)
-        if response_code != 0:
-            return jsonify({"status": "Failed"})
-        return response.json()
-
 
 @app_views.route('/callback', methods=['POST'], strict_slashes=False)
 def mpesa_callback():
--- web_flask/reg.py
+++ web_flask/reg.py
@@ -13,6 +13,7 @@ from web_flask import register
 from base64 import b64encode
 from requests.auth import HTTPBasicAuth
 from . import token_required
+from os import getenv
 
 @register.route('/register', methods=['GET'], strict_slashes=False)
 @token_required('user')
@@ -31,11 +32,67 @@ def register_page():
         return render_template('dashboard.html')
 
 
-@register.route('/pay/', methods=['GET'], strict_slashes=False)
-# @token_required('user')
+# get access token to work with daraja API
+def get_access_token(consumer_key, consumer_secret):
+    """ Get access token to work with daraja API """
+    url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+    response = requests.get(url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+    if response.status_code == 200:
+        return response.json().get('access_token')
+    else:
+        return None
+
+
+@register.route('/pay/', methods=['POST'], strict_slashes=False)
+# @token_required
 def mpesa_express():
     """ This function initiates a payment request to the M-Pesa API. """
+    if request.method == 'POST':
+        access_token = get_access_token(getenv('CONSUMER_KEY'),
+                                        getenv('CONSUMER_SECRET'))
+        time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        password = "174379" + "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" + time_stamp
+        pwd = b64encode(password.encode("utf-8")).decode("utf-8")
+        phone_number = request.form.get('phone_number')
+
+        if not phone_number:
+            flash('Please provide a phone number.', 'error')
+            return render_template('payment.html')
+
+        if not access_token:
+            flash('Payment failed. Please try again.', 'error')
+            return render_template('payment.html')
+
+        headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {access_token}'
+        }
+        payload = {
+        "BusinessShortCode": 174379,
+        "Password": pwd,
+        "Timestamp": time_stamp,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": 1,
+        "PartyA": phone_number,
+        "PartyB": 174379,
+        "PhoneNumber": phone_number,
+        "CallBackURL": "https://epermit.live/api/v1/callback",
+        "AccountReference": "CompanyXLTD",
+        "TransactionDesc": "Payment of X"
+        }
+        payload_json = json.dumps(payload)
+        response = requests.request("POST",
+                                    'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest',
+                                    headers = headers,
+                                    data = payload_json)
+        response_code = response.json().get('ResponseCode')
+        if response_code != 0:
+            flash('Payment failed. Please try again.', 'error')
+            return render_template('payment.html')
+        flash('Payment successful. Your business has been registered.', 'success')
+        return render_template('waiting_page.html')
     return render_template('payment.html')
+
 
 @register.route('/callback', methods=['POST'], strict_slashes=False)
 # @token_required('user')
