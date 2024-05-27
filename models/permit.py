167a65146d0ff408a9d0c4dbd90675ac0212c509
CHIGODI <chiegody254@gmail.com>
2024-05-27 13:56:47 +0300

    Remove r/ship of user and permit

--- models/permit.py
+++ models/permit.py
@@ -14,8 +14,6 @@ class Permit(BaseModel, Base):
     is_valid = Column(Boolean, default=False, nullable=False)
     business_id = Column(String(60), ForeignKey('businesses.id'), nullable=False)
 
-    user = relationship('User', backref='permits')
-
     def __init__(self, *args, **kwargs):
         """ Override the default init method to set the permit number"""
         super().__init__(*args, **kwargs)
