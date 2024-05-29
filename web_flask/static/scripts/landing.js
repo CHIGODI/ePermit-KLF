4ba7ffe982f74f0166f78074bb949931d2d0fa4f
Wambuuyy <wambuiprudy@gmail.com>
2024-05-29 18:40:21 +0300

    remove contact section remove create acc and login button from hero section ude view services in services section add responsiveness to changes in screen size

--- web_flask/static/scripts/landing.js
+++ web_flask/static/scripts/landing.js
@@ -4,27 +4,3 @@ faqs.forEach(faq => {
         faq.classList.toggle("active");
     })
 })
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-
-    // Capture form data
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const subject = document.getElementById('subject').value;
-    const message = document.getElementById('message').value;
-
-    // Optionally, you can send the form data to your server here using fetch or XMLHttpRequest
-
-    // Show the popup message
-    const popupMessage = document.getElementById('popupMessage');
-    popupMessage.style.display = 'block';
-
-    // Hide the popup message after a few seconds
-    setTimeout(function() {
-        popupMessage.style.display = 'none';
-    }, 5000); // Adjust the timeout duration as needed
-
-    // Clear the form fields
-    document.getElementById('contactForm').reset();
-});
